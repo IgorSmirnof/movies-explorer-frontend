e8c1cd3d260f6b43530b03d296745c4fb5e5376f
IGo <igor.smirna@gmail.com>
2023-08-21 19:40:57 +0300

    feat: close of 1280 landing

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,28 +5,31 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer__title">
-        Учебный проект Яндекс.Практикум х BeatFilm.
-      </div>
-      <div className="footer__conteiner">
-        © 2020
-        <div className="footer__links">
-          <Link
-            className="student__link"
-            to={"https://github.com/"}
-            target="_blank"
-            rel="noreferrer"
-          >
-            Яндекс.Практикум
-          </Link>
-          <Link
-            className="student__link"
-            to={"https://github.com/"}
-            target="_blank"
-            rel="noreferrer"
-          >
-            Github
-          </Link>
+      <div className="footer__container">
+        <div className="footer__title">
+          Учебный проект Яндекс.Практикум х BeatFilm.
+        </div>
+
+        <div className="footer__links-wrap">
+          © 2020
+          <div className="footer__links">
+            <Link
+              className="footer__link"
+              to={"https://practicum.yandex.ru/"}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Яндекс.Практикум
+            </Link>
+            <Link
+              className="footer__link"
+              to={"https://github.com/"}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </Link>
+          </div>
         </div>
       </div>
     </footer>
--- src/components/Landing/Hero/Hero.jsx
+++ src/components/Landing/Hero/Hero.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import './Hero.css';
-// import NavButton from '../NavButton/NavButton';
+import React from "react";
+import "./Hero.css";
 
 const Hero = () => {
   return (
-    <section className='hero'>
-      <div className='hero__container'>
-        <h1 className='hero__title'>
-        Учебный проект студента факультета Веб-разработки.
-        </h1>
-        <nav>
-          <ul className='hero__buttons'>
-            <li className='hero__buttons_link'>О проекте</li>
-            <li className='hero__buttons_link'>Технологии</li>
-            <li className='hero__buttons_link'>Студент</li>
-          </ul>
-        </nav>
-        {/* <NavButton /> */}
+    <section className="hero">
+      <div className="hero__container">
+        {/* <div className="hero__wrap"> */}
+          <h1 className="hero__title">
+            Учебный проект студента факультета Веб-разработки.
+          </h1>
+          <nav>
+            <ul className="hero__buttons">
+              <li className="hero__buttons_link">О проекте</li>
+              <li className="hero__buttons_link">Технологии</li>
+              <li className="hero__buttons_link">Студент</li>
+            </ul>
+          </nav>
+        {/* </div> */}
       </div>
     </section>
   );
